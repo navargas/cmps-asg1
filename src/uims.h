20c226dbf2737b5a84465c15d926095692191b9b
Nicholas Vargas <mail@nickvargas.net>
2015-11-28 07:31:08 +0000

    Organize uims struct to place data before functions

--- src/uims.h
+++ src/uims.h
@@ -10,16 +10,16 @@ typedef int hashItem; // placeholder
  * http://stackoverflow.com/a/28535585/3875024
  * Names from assignment pdf*/
 typedef const struct uims {
+  int salt;
+  size_t m; // hash table load
+  int customerId;
+  hashItem* userT; // hash table data
   bool (*isAvailable)(char* uid);
   int (*lookupCustomerId)(char* uid);
   int (*hash)(char* uid);
   void (*generateSalt)(void);
   int (*load)(void);
   void (*reallocate)(void);
-  hashItem* userT; // hash table data
-  size_t m; // hash table load
-  int salt;
-  int customerId;
 } uims;
 
 extern const uims UIMS;
