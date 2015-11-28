992b5e3d0dd4422197be3ca27078a7699e30d3f0
Nicholas Vargas <mail@nickvargas.net>
2015-11-28 07:29:38 +0000

    Add docstring for uims.c

--- src/uims.c
+++ src/uims.c
@@ -1,3 +1,25 @@
+/******************************************************************************/
+/**
+@file		  uims.c
+@author		Nicholas Vargas
+@brief		User ID Management System
+@details
+    Contains functions associated with the storage and verification of
+    user IDs
+@license
+    Licensed under the Apache License, Version 2.0 (the "License");
+		you may not use this file except in compliance with the License.
+		You may obtain a copy of the License at
+			http://www.apache.org/licenses/LICENSE-2.0
+@par
+		Unless required by applicable law or agreed to in writing,
+		software distributed under the License is distributed on an
+		"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+		either express or implied. See the License for the specific
+		language governing permissions and limitations under the
+		License.
+*/
+/******************************************************************************/
 #include <stdlib.h>
 #include "uims.h"
 
